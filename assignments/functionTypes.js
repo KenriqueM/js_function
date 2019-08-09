6dc0fe1d161c521e52f59a1ae86a90f5a4458f3f
Kennedy Maling <kenrique.m24@gmail.com>
2019-08-10 00:58:00 +0530

    update JS Function-Coding style

--- assignments/functionTypes.js
+++ assignments/functionTypes.js
@@ -7,7 +7,7 @@
 
 // Example Start
 /**
- * Converts a number a string.
+ * Converts a number to a string.
  * @param {number} n
  * @return {string} the number as a string
  */
@@ -43,11 +43,57 @@ convertToString(21); // "21"
  * @return {number}
  */
 
+// my code Function Declaration
+
+function addOne(n = 0) {
+	return ++n;
+}
+
+// Function Expression 
+
+let addOne = function addOneInput (n = 0) {
+	return ++n
+};
+
+// Arrow Function without the curly braces
+
+let addOne = n => ++n
+
+// Arrow Function with Curly Braces
+
+let addOne = n => {
+	return ++n
+};
+
+
 /**
  * Subtracts one from a given number.
  * @param {number} n
  * @return {number}
  */
+
+// my code Function Declaration
+
+function subtractOne(n = 0) {
+	return --n;
+}
+
+// Function Expression 
+
+let subtractOne = function subtractOneInput(n = 0) {
+	return --n;
+};
+
+// Arrow Function without the Curly Braces 
+
+let subtractOne = n => --n;
+
+// Arrow Function with the Curly Braces
+
+let subtractOne = n => {
+	return --n
+};
+
 
 /**
  * Adds two numbers.
@@ -56,12 +102,76 @@ convertToString(21); // "21"
  * @return {number} the sum
  */
 
+//my code Function Declaration
+
+function addNumbers( x = 0, y = 0 ) {
+	if (typeof x == "number" && typeof y == "number") {
+		return x + y;
+	} else {
+		alert("not a number");
+	}
+}
+
+// Function Expression
+
+let addNumbers = function addTwoNumbersInput( x = 0, y = 0 ) {
+	if (typeof x == "number" && typeof y == "number") {
+		return x + y;
+	} else {
+	(alert("not a number");
+	}
+};
+
+// Arrow Function without the curly braces
+
+let addNumbers = (x = 0, y = 0) => x + y;
+
+
+// Arrow Function with the Curly Braces
+
+let addNumbers = (x = 0, y = 0) => {
+	return x + y;
+};
+
+
 /**
  * Subtracts the second number from the first.
  * @param {number} x
  * @param {number} y
  * @return {number} the difference
  */
+
+//my code Function Declaration
+
+function subtractSecondNumberFromFirst ( x = 0, y = 0 ) {
+ 	if (typeof x == "number" && typeof y == "number") {
+ 		return y - x;	
+ 	} else {
+ 	 alert("not a number");
+ 	}
+ }
+
+
+// Function Expression
+
+var subtractSecondNumberFromFirst = function ( x = 0, y = 0) {
+	if (typeof x == "number" && typeof y == "number") {
+		return y - x;
+	} else {
+		alert("not a number");
+	}
+};
+
+// Arrow Function without the curly braces
+
+let subtractSecondNumberFromFirst = (x = 0, y = 0) => y - x;
+
+
+// Arrow Function with the Curly brackets
+
+let subtractSecondNumberFromFirst = (x = 0, y = 0) => {
+	return y - x;
+};
 
 /**
  * Multiplies two numbers.
@@ -70,6 +180,37 @@ convertToString(21); // "21"
  * @return {number} the product
  */
 
+//my code Function Declaration 
+
+ function multiplyTwoNumber(x = 0, y = 0) {
+ 	if (typeof x == "number" && typeof y == "number") {
+ 		return (x * y);	
+ 	} else {
+ 		alert("not a number");
+
+ 	}
+ }
+
+// Function Expression 
+
+let multiplyTwoNumber = function multiplyTwoNumbersInput(x = 0, y = 0) {
+	if (typeof x == "number" && typeof y == "number") {
+		return x * y;
+	} else {
+		alert("not a nuumber");
+	}
+}
+
+// Arrow Function without the Curly Braces
+
+let multiplyTwoNumber = (x = 0, y = 0) => x * y;
+
+// Arrow Function with the Curly Braces
+
+let multiplyTwoNumber = (x = 0, y = 0) => {
+	return (x * y);
+}
+
 /**
  * Divides the first number by the second.
  * @param {number} x
@@ -77,11 +218,64 @@ convertToString(21); // "21"
  * @return {number} the quotient
  */
 
+//my code Function Declaration
+
+function divideSecondNumber( x = 0, y = 0 ) {
+	if (typeof x == "number" && typeof == "number") {
+		return y / x;
+	} else {
+		alert("not a number");
+	}
+}
+
+// Function Expression
+
+let divideSecondNumber = function secondNumber( x = 0, y = 0) {
+	if (typeof x == "number" typeof == "number") {
+		return y / x;
+	} else {
+		alert("not a number");
+	}
+};
+
+// Arrow Function without the curly Brackets
+
+let divideSecondNumber = (x = 0, y = 0) => y / x;
+
+// Arrow with the curly brackets
+
+let divideSecondNumber = (x = 0, y = 0) => {
+	return y / x
+};
+
 /**
  * Multiplies a number by itself.
  * @param {number} x, number to be squared
  * @return {number} squared
  */
+
+//my code Function Declaration
+
+function multiplyNumberItself( x = 0) {
+		return (x**2);
+	}
+
+// Function Expression 	
+
+var multiplyNumberItself = function multiplyItself( x = 0) {
+	return (x**2);
+};
+
+// Arrow Function without the curly brackets
+
+var multiplyNumberItself = (x = 0) => x**2;
+
+//Arrow Function without the Curly Brackets
+
+var multiplyNumberItself = (x = 0) => {
+	return x**2;
+}
+
 
 /**
  * Performs a mathematical operation on two numbers.
@@ -92,12 +286,41 @@ convertToString(21); // "21"
  * @return {number} the result
  */
 
+//my code Function Declaration
+
+//let operation = "add", "subtract", "multiply", "divide";
+
+function performMathOperation (x, y, operation) {
+	if (operation == "+") {
+		return x + y;
+	} else if (operation == "-") {
+		return x - y;
+	} else if (operation == "*") {
+		return x * y;
+	} else if (operation == "/") {
+		return x / y;
+	} else {
+		return "none";
+	}
+}
+
+
 /**
  * Returns true if `a` is greater than `b`.
  * @param {number} a
  * @param {number} b
  * @return {boolean} `a` is larger than `b`
  */
+
+//my code Function Declaration 
+
+function greaterOrLarger( a, b) {
+	if (a > b) {
+		return true;
+} else {
+	return false;
+}
+}
 
 /**
  * Returns true if `a` is less than `b`.
@@ -106,12 +329,32 @@ convertToString(21); // "21"
  * @return {boolean} `a` is smaller than `b`
  */
 
+//my code Function Declaration
+
+function performTrueFalse( a, b) {
+	if (a < b) {
+		return true; 
+} else { 
+		return false;
+}
+}
+
 /**
  * Returns true if `a` and `b` are equal.
  * @param {number} a
  * @param {number} b
  * @return {boolean} the numbers are equal
  */
+
+//my code Function Declaration
+
+function equalNumbers( a = 0, b =0 ) {
+	if (a == b) {
+		return "the numbers are equal";
+	} else {
+		return "the numbers are not equal"
+	}
+}
 
 /**
  * Returns the smallest value of two numbers.
@@ -120,6 +363,18 @@ convertToString(21); // "21"
  * @return {number} the smallest number
  */
 
+//my code Function Declaration
+
+function smallestValue(x = 0, y =0) {
+	if (x < y) {
+		return x;
+	} else if (y < x) {
+		return y;
+	} else {
+		return "";
+	}
+}
+
 /**
  * Returns the largest value of two numbers.
  * @param {number} x
@@ -127,17 +382,48 @@ convertToString(21); // "21"
  * @return {number} the largest number
  */
 
+//my code Function Decalaration
+
+function largestValue(x=0 , y=0) {
+	if (x > y) {
+		return x;
+	} else if (y > x) 
+		return y;
+	}
+}
+
 /**
  * Returns true if `n` is even.
  * @param {number} n
  * @return {boolean} the number is even
  */
 
+//my code
+
+let n = 0;
+ function getEvenNumber(n =0) {
+ 	if (n % 2 == 0) {
+ 		return true;
+ 	} else {
+ 		return false;
+ 	}
+ }
+
 /**
  * Returns true if `n` is odd.
  * @param {number} n
  * @return {boolean} the number is odd
  */
+
+//my code Function Expression
+
+var getOddNumberOut = function getOddNumber(n = 0) {
+	if (n%2==0) {
+		return "odd";
+	} else {
+		return "even";
+	}
+};
 
 /**
  * Returns a letter grade.
@@ -151,9 +437,41 @@ convertToString(21); // "21"
  * @return {string} the score represented as a letter grade
  */
 
+//my code
+
+function calculateGrades(score, total) {
+	let percentage = (score * 100) / total
+	switch (true) {
+		case (percentage < 60):
+		return "F";
+		break;
+		case (percentage >= 60 && percentage < 69):
+		return "D";
+		break;
+		case (percentage >= 70 && percentage < 79):
+		return "C";
+		break;
+		case (percentage >= 80 && percentage < 89):
+		return "B";
+		break;
+		case (percentage >= 90 && percentage < 100):
+		return "A";
+		break;
+		default:
+		return "check with your teacher";
+
+	}
+}
 /**
  * Joins two strings with a space.
  * @param {string} word1
  * @param {string} word2
  * @return {string} joined the words joined with a space
  */
+
+//my code
+
+function stringsWithSpace(word1, word2) {
+	return (word1+ " " +word2);
+}
+
--- assignments/functions.md
+++ assignments/functions.md
@@ -1,31 +1,47 @@
 1. 🎖Write a function named calculateDogAge that:
-  * [ ] Takes 1 argument: your puppy's age.
-  * [ ] Calculates your dog's age based on the conversion rate of 1 human year to 7 dog years.
-  * [ ] Outputs the result to the screen like so: "Your doggie is NN years old in dog years!"
-  * [ ] Add an additional argument to the function that takes the conversion rate of human to dog years.
+
+- [ ] Takes 1 argument: your puppy's age.
+- [ ] Calculates your dog's age based on the conversion rate of 1 human year to 7 dog years.
+- [ ] Outputs the result to the screen like so: "Your doggie is NN years old in dog years!"
+- [ ] Add an additional argument to the function that takes the conversion rate of human to dog years.
 
 ```js
 // your code goes here
+
+
 ```
+
 2. 🎖Write a function named calculateSupply that:
-  * [ ] takes 2 arguments: age, amount per day.
-  * [ ] calculates the amount consumed for rest of the life (based on a constant max age).
-  * [ ] outputs the result to the screen like so: "You will need NN to last you until the ripe old age of X"
-  * [ ] Accept floating point values for amount per day, and round the result to a round number.
+
+- [ ] takes 2 arguments: age, amount per day.
+- [ ] calculates the amount consumed for rest of the life (based on a constant max age).
+- [ ] outputs the result to the screen like so: "You will need NN to last you until the ripe old age of X"
+- [ ] Accept floating point values for amount per day, and round the result to a round number.
 
 ```js
 // your code goes here
+
+function calculateSupply(age, amount per day) {
+
+}
+
 ```
+
 3. 🎖Create a function called celsiusToFahrenheit:
-  * [ ] Store a celsius temperature into a variable.
-  * [ ] Convert it to fahrenheit and output "NN°C is NN°F".
-  * [ ] Create a function called fahrenheitToCelsius:
-  * [ ] Now store a fahrenheit temperature into a variable.
-  * [ ] Convert it to celsius and output "NN°F is NN°C."
+
+- [ ] Store a celsius temperature into a variable.
+- [ ] Convert it to fahrenheit and output "NN°C is NN°F".
+- [ ] Create a function called fahrenheitToCelsius:
+- [ ] Now store a fahrenheit temperature into a variable.
+- [ ] Convert it to celsius and output "NN°F is NN°C."
 
 ```js
 // your code goes here
+
+
+
 ```
+
 4. 🎖The function below returns true if the parameter age is greater than 18. Otherwise it asks for a confirmation and returns its result:
 
 ```js
@@ -38,15 +54,23 @@ function checkAge(age) {
   }
 }
 ```
-  4.1 🎖Convert the above function using ternary operator.
-  ```js
-  // your code goes here
-  ```
 
-  4.2 🎖Convert the above function using `||` operator.
-  ```js
-  // your code goes here
-  ```
+4.1 🎖Convert the above function using ternary operator.
+
+```js
+// your code goes here
+
+
+```
+
+4.2 🎖Convert the above function using `||` operator.
+
+```js
+// your code goes here
+
+(age > 18) (return true) || (age < 18) (return confirm("Did parents allow you?");)
+```
+
 Will the function work differently if else is removed like below?
 
 ```js
@@ -56,17 +80,19 @@ function checkAge(age) {
   }
   // ...
   return confirm("Did parents allow you?");
-}
+} 
+
+// NO
 ```
 Is there any difference in the behavior of these two variants? If there is what is that?
 
 
 5. 🎖 Write a function pow(x,n) that returns x in power n.
 
-  * [ ] Use prompt to take values for x and n, and then shows the result of pow(x,n) using alert.
-  * [ ] In this task the function should support only natural values of n: integers greater then 1.
+- [ ] Use prompt to take values for x and n, and then shows the result of pow(x,n) using alert.
+- [ ] In this task the function should support only natural values of n: integers greater then 1.
 
-```js
+````js
 // Your code goes here
 
 // After writing code uncomment to check the answer.
@@ -79,12 +105,14 @@ Is there any difference in the behavior of these two variants? If there is what 
 
 ```js
 // your code goes here
-```
+````
+
 6. 🎖Write a program that asks the user for a number n using prompt and prints the sum of the numbers 1 to n
 
 ```js
 // your code goes here
 ```
+
 7. 🎖Modify the previous program such that only multiples of 5 or 7 are considered in the sum, e.g. n = 20 (5,7,10,14,15,20) 71
 
 ```js
